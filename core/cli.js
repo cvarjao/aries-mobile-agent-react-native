2ca12dc7e514fbe16da83475090c17ed1cbbba05
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2022-05-07 19:58:08 -0700

    generator
    Signed-off-by: Clécio Varjão <1348549+cvarjao@users.noreply.github.com>

--- core/cli.js
+++ core/cli.js
@@ -2,6 +2,41 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
 
+const fs = require('fs');
+const path = require('path');
+
+function copyFileSync( source, target ) {
+
+  const targetFile = target;
+
+  // If target is a directory, a new file with the same name will be created
+  if ( fs.existsSync( target ) ) {
+      if ( fs.lstatSync( target ).isDirectory() ) {
+          targetFile = path.join( target, path.basename( source ) );
+      }
+  }
+
+  fs.writeFileSync(targetFile, fs.readFileSync(source));
+}
+
+function listFilesRecursive( root, sourceDir ) {
+  const output = []
+  const source = sourceDir || root;
+
+  // Copy
+  if ( fs.lstatSync( source ).isDirectory() ) {
+      const files = fs.readdirSync( source )
+      for (const file of files) {
+        var curSource = path.join( source, file );
+        output.push(...listFilesRecursive( root, curSource ))
+      }
+  } else{
+    const _src = path.relative(root, source).split(path.sep).join(path.posix.sep)
+    output.push({src: _src, dst: _src })
+  }
+  return output
+}
+
 function syncPackageJson() {
   const { execSync } = require('child_process')
   const fs = require('fs')
@@ -89,12 +124,67 @@ function syncPackageJson() {
   )
   */
 }
+async function generateAppBoilerplace(args) {
+  const { once } = require('events');
+  const readline = require('readline');
+  const sourceDir = path.resolve(__dirname, './template')
+  const app = require(path.resolve(process.cwd(), 'app.json'))
+  const copySpecs = listFilesRecursive(sourceDir)
+  .map((item)=>{
+    item.dst = item.dst.replace(new RegExp('^android/app/src/main/java/com/ariesbifold/'), `android/app/src/main/java/${app.package.split('.').join('/')}/`)
+    item.dst = item.dst.replace(new RegExp('^android/app/src/debug/java/com/ariesbifold/'), `android/app/src/debug/java/${app.package.split('.').join('/')}/`)
+    if (item.dst.startsWith('ios/')) {
+      item.dst = item.dst.replace(new RegExp('AriesBifold', 'g'), app.name)
+    }
+    return item
+  })
+  
+  const targetDir =  path.resolve(process.cwd())
+  for (const copySpec of copySpecs) {
+    const srcPath = path.join(sourceDir, copySpec.src)
+    const dstPath = path.join(targetDir, copySpec.dst)
+    const dstDirPath = path.dirname(dstPath)
+    if (!fs.existsSync(dstDirPath)) {
+      fs.mkdirSync(dstDirPath, {recursive: true})
+    }
+    if (
+      copySpec.src.endsWith('.java') || 
+      copySpec.src.endsWith('strings.xml') || 
+      copySpec.src.endsWith('.gradle') || 
+      copySpec.src.endsWith('BUCK') || 
+      copySpec.src.endsWith('AndroidManifest.xml') ||
+      (copySpec.src.endsWith('Info.plist') && !copySpec.src.endsWith('Indy.framework/Info.plist')) ||
+      copySpec.src.endsWith('AppDelegate.m') ||
+      copySpec.src.endsWith('project.pbxproj')
+    ) {
+      var outputSream = fs.createWriteStream(dstPath, {
+        flags: 'w'
+      })
+      const rl = readline.createInterface({
+        input: fs.createReadStream(srcPath),
+        terminal: false
+      });
+      rl.on('line', (line) => {
+        outputSream.write(line.toString().replace(new RegExp('com\.ariesbifold'), app.package).replace(new RegExp('org\.reactjs\.native\.example\.AriesBifold'), app.package).replace(new RegExp('aries-bifold'), app.name).replace(new RegExp('(?<!-)AriesBifold(?!-)'), app.name).replace(new RegExp('Aries Bifold'), app.displayName) + "\n")
+      });
+      await once(rl, 'close');
+      outputSream.end()
+      outputSream.close()
+      //fs.copyFileSync(srcPath, dstPath)
+    }else {
+      fs.copyFileSync(srcPath, dstPath)
+    }
+  }
+}
+
 module.exports = {
   cli: (_args) => {
     const args = _args.splice(2)
     console.log(args)
     if (args[0] === 'sync-package-json') {
       syncPackageJson()
+    }else if (args[0] === 'generate-app-boilerplate') {
+      generateAppBoilerplace(args.splice(1))
     }
   },
 }
